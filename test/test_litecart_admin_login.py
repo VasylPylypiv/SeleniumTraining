7d0b4d9fdf9faccb420c55f0528880710f032b95
vasyl.pylypiv <strange.viking@gmail.com>
2017-06-13 10:11:40 +0300

    Local installed Litecart admin page login test

--- test/test_litecart_admin_login.py
+++ test/test_litecart_admin_login.py
@@ -3,8 +3,8 @@ from selenium import webdriver
 
 def test_litecart_admin_login():
     driver = webdriver.Firefox()
-    driver.get("http://demo.litecart.net/admin/login.php")
-    driver.find_element_by_name('username').send_keys('demo')
-    driver.find_element_by_name('password').send_keys('demo')
+    driver.get("http://localhost/litecart/admin/")
+    driver.find_element_by_name('username').send_keys('admin')
+    driver.find_element_by_name('password').send_keys('admin')
     driver.find_element_by_class_name('btn.btn-default').click()
     driver.quit()
