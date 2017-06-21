a8e75b3022db06a337fdd99843e9a19cc71945ca
vasyl.pylypiv <strange.viking@gmail.com>
2017-06-21 19:07:12 +0300

    Task 5 completed

---
+++ test/test_task5.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+
+
+def test_litecart_admin_login():
+    ff = webdriver.Firefox()
+    ff.implicitly_wait(5)
+    ff.get("http://localhost/litecart/")
+    main_page_name = ff.find_element_by_xpath("//div[@class='name']").text
+    main_page_rprice = ff.find_element_by_xpath("//s[@class='regular-price']").text
+    main_page_dprice = ff.find_element_by_xpath("//strong[@class='campaign-price']").text
+    main_page_price_color = ff.find_element_by_xpath("//strong[@class='campaign-price']").value_of_css_property('color')
+    main_page_font_weight = ff.find_element_by_xpath("//strong[@class='campaign-price']").value_of_css_property('font-weight')
+    ff.find_element_by_xpath("(//*[@title='Yellow Duck'])").click()
+    item_page_name = ff.find_element_by_xpath("//h1[@class='title']").text
+    item_page_rprice = ff.find_element_by_xpath("//del[@class='regular-price']").text
+    item_page_dprice = ff.find_element_by_xpath("//strong[@class='campaign-price']").text
+    item_page_price_color = ff.find_element_by_xpath("//strong[@class='campaign-price']").value_of_css_property('color')
+    item_page_font_weight = ff.find_element_by_xpath("//strong[@class='campaign-price']").value_of_css_property('font-weight')
+    print(main_page_name == item_page_name)
+    print(main_page_rprice == item_page_rprice)
+    print(main_page_dprice == item_page_dprice)
+    print(main_page_price_color == item_page_price_color)
+    print(main_page_font_weight == item_page_font_weight)
+    ff.quit()
+
